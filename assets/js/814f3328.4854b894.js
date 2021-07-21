c71fae98c63f3dfa44b70d0f6d69fd38f4464895
Ye Myint Oo <30695023+Ye-Myint-Oo@users.noreply.github.com>
2021-07-21 21:43:03 +0800

    Deploy website - based on

--- assets/js/814f3328.4854b894.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkyemyintoo_blog=self.webpackChunkyemyintoo_blog||[]).push([[535],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/First Day of My Personal Blog"}]}')}}]);
